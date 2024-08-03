3fa7ed4e91114a52eeabb03e89d40e2963e1bcf6
kangciu <a2849535@gmail.com>
2024-08-03 23:00:37 +0900

    feat: 메인 탭 기초 공사
    디자인시스템에셋이 적용이 안되는 오류 있음

---
+++ Projects/Feature/MainFeature/Sources/Presentation/Model/MainModel.swift
@@ -0,0 +1,9 @@
+//
+//  MainModel.swift
+//  MainFeature
+//
+//  Created by 강치우 on 8/3/24.
+//  Copyright © 2024 Svanguard. All rights reserved.
+//
+
+import Foundation
--- Projects/MainTabFeature/Sources/Presentation/MainTabView.swift
+++ Projects/MainTabFeature/Sources/Presentation/MainTabView.swift
@@ -9,11 +9,45 @@ import SwiftUI
 import MainFeature
 
 public struct MainTabView: View {
-    public init() { }
+    // MARK: 네비게이션, 탭바 디폴트 색상
+    public init() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithDefaultBackground()
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithDefaultBackground()
+        UITabBar.appearance().standardAppearance = tabBarAppearance
+        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+    }
     
     public var body: some View {
-        ZStack {
-            ContentView()
-        }.background(.blue)
+        TabView {
+            MainView()
+                .tabItem {
+                    Label("전과자", systemImage: "person.slash")
+                }
+            
+            SampleView()
+                .tabItem {
+                    Label("핵의심", systemImage: "magnifyingglass")
+                }
+            
+            SampleView2()
+                .tabItem {
+                    Label("제보하기", systemImage: "light.beacon.max")
+                }
+            
+            SampleView3()
+                .tabItem {
+                    Label("설정", systemImage: "gearshape")
+                }
+        }
+        .tint(.primary)
     }
+}
+
+#Preview {
+    MainTabView()
 }
