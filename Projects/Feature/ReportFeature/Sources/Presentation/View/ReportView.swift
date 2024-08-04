144a684b097be5c0499f46bddb1c580cae89564b
kangciu <a2849535@gmail.com>
2024-08-05 01:11:04 +0900

    feat: ReportView 탭뷰 작업

--- Projects/Feature/ReportFeature/Sources/Presentation/View/ReportView.swift
+++ Projects/Feature/ReportFeature/Sources/Presentation/View/ReportView.swift
@@ -14,52 +14,68 @@ public struct ReportView: View {
     @State private var expandedSections: Set<UUID> = []
     
     private var items = ReportModel.items
+    private var image = exampleImage.item
     
     public var body: some View {
         NavigationStack {
-            List {
-                Section {
-                    ForEach(items) { item in
-                        DisclosureGroup(
-                            isExpanded: Binding(
-                                get: { expandedSections.contains(item.id) },
-                                set: { isExpanded in
-                                    if isExpanded {
-                                        expandedSections.insert(item.id)
-                                    } else {
-                                        expandedSections.remove(item.id)
+            VStack {
+                TabView {
+                    ForEach(image) { exampleImage in
+                        Image(uiImage: exampleImage.imageName)
+                            .resizable()
+                            .cornerRadius(10)
+                            .scaledToFit()
+                            .clipped()
+                    }
+                }
+                .tabViewStyle(.page(indexDisplayMode: .automatic))
+                .frame(height: 220)
+                .padding([.top, .horizontal])
+                
+                List {
+                    Section {
+                        ForEach(items) { item in
+                            DisclosureGroup(
+                                isExpanded: Binding(
+                                    get: { expandedSections.contains(item.id) },
+                                    set: { isExpanded in
+                                        if isExpanded {
+                                            expandedSections.insert(item.id)
+                                        } else {
+                                            expandedSections.remove(item.id)
+                                        }
+                                    }
+                                ),
+                                content: {
+                                    Text(item.subtitle)
+                                        .font(.subheadline)
+                                },
+                                label: {
+                                    HStack {
+                                        Image(systemName: "questionmark")
+                                        
+                                        Text(item.title)
+                                            .font(.headline)
                                     }
                                 }
-                            ),
-                            content: {
-                                Text(item.subtitle)
-                                    .font(.subheadline)
-                            },
-                            label: {
-                                HStack {
-                                    Image(systemName: "questionmark")
-                                    
-                                    Text(item.title)
-                                        .font(.headline)
-                                }
-                            }
-                        )
+                            )
+                        }
+                    } header: {
+                        Text("자주 묻는 질문 (FAQ)")
                     }
-                } header: {
-                    Text("자주 묻는 질문 (FAQ)")
+                    .listSectionSeparator(.hidden)
                 }
-                .listSectionSeparator(.hidden)
+                .tint(.primary)
+                .listStyle(.inset)
+                .navigationTitle("제보하기")
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarItems(trailing: Button {
+                    
+                } label: {
+                    Text("제보")
+                        .foregroundStyle(.red)
+                })
             }
-            .tint(.primary)
-            .listStyle(.inset)
-            .navigationTitle("제보하기")
-            .navigationBarTitleDisplayMode(.inline)
-            .navigationBarItems(trailing: Button {
-                
-            } label: {
-                Text("제보")
-                    .foregroundStyle(.red)
-            })
         }
     }
 }
