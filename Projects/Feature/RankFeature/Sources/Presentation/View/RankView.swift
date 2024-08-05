80a938ccf7eeb500e68a29737c9664f6ebe8e7be
kangciu <a2849535@gmail.com>
2024-08-06 07:20:04 +0900

    feat: 검색 기능(필터링) 구현

--- Projects/Feature/RankFeature/Sources/Presentation/View/RankView.swift
+++ Projects/Feature/RankFeature/Sources/Presentation/View/RankView.swift
@@ -15,7 +15,7 @@ public struct RankView: View {
     @State private var text: String = ""
     @State private var isEditing: Bool = false
     
-    // 임시 데이터 배열
+    // MARK: 임시 데이터 배열
     private let users = [
         ("이창준", "2453454241"),
         ("강치우", "1231241352"),
@@ -28,53 +28,45 @@ public struct RankView: View {
         ("황성진", "3452342345"),
     ]
     
+    // MARK: 필터링
+    private var filteredUsers: [(String, String)] {
+        if text.isEmpty {
+            return users
+        } else {
+            return users.filter { $0.0.contains(text) || $0.1.contains(text) }
+        }
+    }
+    
     public var body: some View {
         NavigationStack {
-            VStack {
-                HStack {
-                    Image(systemName: "magnifyingglass")
-                        .foregroundColor(.primary)
-                    
-                    TextField("검색", text: $text)
-                        .foregroundColor(.primary)
-                        .tint(.primary)
-                        .submitLabel(.search)
-                        .onTapGesture {
-                            withAnimation {
-                                isEditing = true
-                            }
+            if #available(iOS 17.0, *) {
+                VStack {
+                    List(filteredUsers, id: \.0) { user in
+                        Section {
+                            RankUserListCell(surfingID: user.0, surfingUsername: user.1)
                         }
-                        .overlay {
-                            HStack {
-                                Spacer()
-                                
-                                if isEditing && !text.isEmpty {
-                                    Button(action: {
-                                        text = ""
-                                    }) {
-                                        Image(systemName: "xmark.circle.fill")
-                                            .foregroundColor(Color.gray)
-                                            .frame(width: 35, height: 35)
-                                    }
-                                    .padding(.trailing, 5)
-                                }
-                            }
+                        .listSectionSeparator(.hidden, edges: .top)
+                    }
+                    .listStyle(.plain)
+                    .searchable(text: $text, placement: .navigationBarDrawer(displayMode: .always), prompt: "검색")
+                }
+                .navigationTitle("실시간랭킹")
+                // MARK: 이게 너무 쓰고싶은데 17버전에서만 지원해서 쩔수없이 버전 분기처리함 ㅠ.ㅠ
+                .toolbarTitleDisplayMode(.inlineLarge)
+            } else {
+                VStack {
+                    List(filteredUsers, id: \.0) { user in
+                        Section {
+                            RankUserListCell(surfingID: user.0, surfingUsername: user.1)
                         }
+                        .listSectionSeparator(.hidden, edges: .top)
+                    }
+                    .listStyle(.plain)
+                    .searchable(text: $text, placement: .navigationBarDrawer(displayMode: .always), prompt: "검색")
                 }
-                .padding(10)
-                .background(Color(.systemGray6))
-                .cornerRadius(12)
-                .padding(.top, 20)
-                .padding(.horizontal)
-                
-                List(users, id: \.0) { user in
-                    RankUserListCell(surfingID: user.0, surfingUsername: user.1)
-                }
-                .listStyle(.plain)
-                .padding(.top, 8)
+                .navigationTitle("실시간랭킹")
+                .navigationBarTitleDisplayMode(.large)
             }
-            .navigationTitle("실시간랭킹")
-            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
