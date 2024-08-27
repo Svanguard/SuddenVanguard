966e8b5d336c1b5ca5074ec8582fdb1b09388de7
DongHo Choe <hamfan524@naver.com>
2024-08-27 17:26:30 +0900

    design: 메인화면 수정
    닉네임, 병영번호로 검색하는 창 2개로 변경

--- Projects/Feature/MainFeature/Sources/Presentation/View/MainView.swift
+++ Projects/Feature/MainFeature/Sources/Presentation/View/MainView.swift
@@ -31,12 +31,31 @@ public struct MainView: View {
                         .fontWeight(.regular)
                         .padding(.top, 10)
                     
-                    NavigationLink {
-                        UserSearchView()
-                    } label: {
-                        SearchFieldView()
+                    HStack {
+                        Spacer()
+                        
+                        NavigationLink {
+                            UserSearchView()
+                        } label: {
+                            SearchFieldView(
+                                placeHolder: "닉네임으로 검색",
+                                opacityValue: 1.0
+                            )
+                        }
+                        
+                        Spacer()
+                        
+                        NavigationLink {
+                            UserSearchView()
+                        } label: {
+                            SearchFieldView(
+                                placeHolder: "병영번호로 검색",
+                                opacityValue: 0.2
+                            )
+                        }
+                        
+                        Spacer()
                     }
-                    
                 }
                 .padding(.horizontal, 20)
                 .padding(.top, 50)
