aa770fe09f6868961b89c61e661fff54202332a6
kangciu <a2849535@gmail.com>
2024-09-17 01:25:43 +0900

    fix: Common에서 DesignSystem 의존 못하는 오류 수정

--- Projects/Common/Sources/Presentation/Model/Tab.swift
+++ Projects/Common/Sources/Presentation/Model/Tab.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2024 Svanguard. All rights reserved.
 //
 
-import DesignSystem
 import SwiftUI
 
 public enum Tab: String {
@@ -14,17 +13,4 @@ public enum Tab: String {
     case rank = "실시간순위"
     case report = "제보하기"
     case setting = "설정"
-    
-    public var symbolImage: Image {
-        switch self {
-        case .main:
-            return Image(uiImage: DesignSystemAsset.vanguard.image)
-        case .rank:
-            return Image(uiImage: DesignSystemAsset.flame.image)
-        case .report:
-            return Image(uiImage: DesignSystemAsset.mail.image)
-        case .setting:
-            return Image(systemName: "gearshape")
-        }
-    }
 }
